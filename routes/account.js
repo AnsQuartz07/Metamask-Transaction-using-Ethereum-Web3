6b74cea920888521153f703fe22224f6f62dea3a
Anshu Kumar <97735640+AnsQuartz07@users.noreply.github.com>
2022-03-16 18:02:07 +0530

    Update and rename account.js to metamask.js

--- routes/account.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const controller = require('../Controller/controller.js')
-
-router.get('/transaction', controller.transaction)
-router.get('/balance', controller.balance)
-
-
-module.exports = router;
